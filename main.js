78584cbd3545d091bab92d4b4265b9c4db7fb729
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2024-01-18 17:29:24 +0100

    Updated footer.css

--- main.js
+++ main.js
@@ -7,8 +7,15 @@ document.body.onload = () => {
         console.log("Yauuuuuuuuuuuuuuuuuu");
     }, 5);
     
+    let b = new ajaxUtilities.SimpleAjaxRequest("GET", "/components/footer/footer.html", (x) => {
+        console.log(x.responseText);
+        let tmp = document.createElement("template");
+        tmp.innerHTML = x.responseText;
+        document.body.appendChild(tmp.content);
+        console.log("Yauuuuuuuuuuuuuuuuuu");
+    }, 5);
+
     a.fire();
+    b.fire();
     console.log("Daje Roma");
 }
-
-
