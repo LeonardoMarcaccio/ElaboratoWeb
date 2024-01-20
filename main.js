d971f00b925b09c172cbfe4fb503fd9718b6429e
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-01-20 17:03:50 +0100

    Fixed error

--- main.js
+++ main.js
@@ -7,13 +7,6 @@ function mainPageInit(array) {
     });
 }
 
-function mainPageInit(array) {
-    array.forEach(async element => {
-        let e = await AssetManager.loadAsset(element);
-        DOMUtilities.addChildElementToNode(document.body, e);
-    });
-}
-
 document.body.onload = () => {
    mainPageInit(["header.html", "footer.html"]);
 }
