7808e812ccfe83ff3b0db17c8a560dc6badcc0d0
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2024-01-20 16:11:58 +0100

    Added function loadElements

--- main.js
+++ main.js
@@ -4,18 +4,34 @@ document.body.onload = () => {
         let tmp = document.createElement("template");
         tmp.innerHTML = x.responseText;
         document.body.appendChild(tmp.content);
-        console.log("Yauuuuuuuuuuuuuuuuuu");
+        console.log("OK");
     }, 5);
-    
-    let b = new ajaxUtilities.SimpleAjaxRequest("GET", "/components/footer/footer.html", (x) => {
+
+    let b = new ajaxUtilities.SimpleAjaxRequest("GET", "/components/profile/profile.html", (x) => {
         console.log(x.responseText);
         let tmp = document.createElement("template");
         tmp.innerHTML = x.responseText;
         document.body.appendChild(tmp.content);
-        console.log("Yauuuuuuuuuuuuuuuuuu");
+        console.log("OK");
     }, 5);
 
     a.fire();
     b.fire();
+
+    //loadElements(["/components/header/header.html", "/components/footer/footer.html"]);
     console.log("Daje Roma");
+}
+
+function loadElements(array) {
+    array.forEach(element => {
+        let e = new ajaxUtilities.SimpleAjaxRequest("GET", element, (x) => {
+            console.log(x.responseText);
+            let tmp = document.createElement("template");
+            tmp.innerHTML = x.responseText;
+            document.body.appendChild(tmp.content);
+            console.log("OK");
+        }, 5);
+
+        e.fire();
+    });
 }
