ca86feb217a245ba396cb199bed568d621d21fbb
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-01-20 16:58:34 +0100

    Main script cleanup

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 let assetPrototypes = new Map();
 
-async function mainPageInit() {
+async function mainPageHeaderInit() {
     let header = await AssetManager.loadAsset("header.html");
     DOMUtilities.addChildElementToNode(document.body, header)
+}
 
+function mainPageContentInit() {
+    let mainContentPage = document.createElement("div");
+    mainContentPage.id = 'main-content-page';
+    document.body.appendChild(mainContentPage);
+}
+
+async function mainPageFooterInit() {
     let footer = await AssetManager.loadAsset("footer.html");
     DOMUtilities.addChildElementToNode(document.body, footer)
+}
+
+async function mainPageInit() {
+    await mainPageHeaderInit();
+    mainPageContentInit();
+    await mainPageFooterInit();
+    document.getElementById("loading-banner").style.display = "none";
 }
 
 document.body.onload = () => {
