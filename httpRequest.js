15be790a00162d7058012b88fccd380d7dbf7dd7
Leonardo Marcaccio <Leonardo Marcaccio>
2024-01-23 16:55:16 +0100

    Added Http Request template

---
+++ httpRequest.js
@@ -0,0 +1,17 @@
+
+function XHRRequest() {
+    const xhr = new XMLHttpRequest();
+    
+    xhr.open('POST', 'https://example.com/endpoint');
+    xhr.setRequestHeader('Accept');
+    xhr.onload = handleResponse;
+    xhr.send()
+}
+
+function handleResponse() {
+    if (xhr.status === 200) {
+      console.log('Response:', JSON.parse(xhr.responseText));
+    } else {
+      console.error('Error:', xhr.statusText);
+    }
+}  
