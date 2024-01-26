46425005e06a6a8fd7c797fba716a06e249be4c5
Leonardo Marcaccio <Leonardo Marcaccio>
2024-01-26 17:57:09 +0100

    Added getter and setters to user

--- api/utility/user.php
+++ api/utility/user.php
@@ -30,10 +30,85 @@
             $this->lastName=$lastName;
             $this->gender=$gender;
             $this->biography=$biography;
-            $this->biography=$biography;
             $this->personalWebsite=$personalWebsite;
             $this->pfp=$pfp;
             $this->phoneNumbers=$phoneNumbers;
+        }
+
+        public function getUsername() {
+            return $this->username;
+        }
+
+        public function getEmail() {
+            return $this->email;
+        }
+
+        public function setEmail($email) {
+            $this->email = $email;
+        }
+
+        public function getPassword() {
+            return $this->password;
+        }
+
+        public function setPassword($password) {
+            $this->password = $password;
+        }
+
+        public function getFirstName() {
+            return $this->firstName;
+        }
+
+        public function setFirstName($firstName) {
+            $this->firstName = $firstName;
+        }
+
+        public function getLastName($lastName) {
+            return $this->lastName;
+        }
+
+        public function setLastName($lastName) {
+            $this->lastName = $lastName;
+        }
+
+        public function getGender() {
+            return $this->gender;
+        }
+
+        public function setGender($gender) {
+            $this->gender = $gender;
+        }
+
+        public function getBiography() {
+            return $this->biography;
+        }
+
+        public function setBiography($biography) {
+            $this->biography = $biography;
+        }
+
+        public function getPersonalWebsite() {
+            return $this->personalWebsite;
+        }
+
+        public function setPersonalWebsite($personalWebsite) {
+            $this->personalWebsite = $personalWebsite;
+        }
+
+        public function getPfp() {
+            return $this->pfp;
+        }
+
+        public function setPfp($pfp) {
+            $this->pfp = $pfp;
+        }
+
+        public function getPhoneNumbers() {
+            return $this->phoneNumbers;
+        }
+
+        public function setPhoneNumbers($phoneNumbers) {
+            $this->phoneNumbers = $phoneNumbers;
         }
     }
 ?>
