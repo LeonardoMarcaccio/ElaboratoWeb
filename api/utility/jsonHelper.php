533977afa2264c7818f211c9187dc7144494b60b
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-01-26 19:20:50 +0100

    Edited jsonHelper functionality

--- api/utility/jsonHelper.php
+++ api/utility/jsonHelper.php
@@ -1,31 +1,15 @@
 <?php
-  function UserToJSON(    //NOSONAR
-    $username,
-    $email,
-    $password,
-    $firstName,
-    $lastName,
-    $gender,
-    $biography,
-    $personalWebsite,
-    $pfp,
-    $phoneNumbers
-    ) {
-      $user = new User(
-        $username,
-        $email,
-        $password,
-        $firstName,
-        $lastName,
-        $gender,
-        $biography,
-        $personalWebsite,
-        $pfp,
-        $phoneNumbers
-      );
-      
-      return json_encode($user);
+  function jSONtoUser($jsonString) {
+    $assArray = json_decode($jsonString, true);
+    if ($assArray !== null) {
+      return new User($assArray['username'], $assArray['email'],
+      $assArray['password'], $assArray['firstName'], $assArray['lastName'],
+      $assArray['gender'], $assArray['biography'], $assArray['personalWebsite'],
+      $assArray['pfp'], $assArray['phoneNumbers']);
+    } else {
+      throw new Error("Unauthorized", 401);
     }
+  }
     
   function generateJSONResponse($code, $message, $dataName = null, $data = null) {
     $jsonObject = new stdClass();
