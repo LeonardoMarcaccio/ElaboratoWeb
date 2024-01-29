21bc35eff073a13e53f80ffbd9451b800b72de40
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-01-29 22:47:39 +0100

    Added function to generate token in safetyutils

--- api/utility/safetyutils.php
+++ api/utility/safetyutils.php
@@ -2,3 +2,7 @@
   function sanitizeInput($usrInput) {
     return htmlspecialchars(strip_tags($usrInput), ENT_QUOTES, 'UTF-8');
   }
+  function generateToken($length = 32) {
+    $randomBytes = random_bytes($length);
+    return bin2hex($randomBytes);
+  }
