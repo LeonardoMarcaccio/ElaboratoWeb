43a58ff7f841b79dec2839fa2eac54c36d1bd48e
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-01-29 22:48:21 +0100

    Continued work on registration api

--- api/auth/registration.php
+++ api/auth/registration.php
@@ -6,28 +6,38 @@
 
   try {
     assertRequestMatch('POST');
-    try {
-      $usrObj = jSONtoUser(file_get_contents("php://input"));
-      $report = performCredentialReport($usrObj);
-      if ($report->allTestPassed()) {
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-        $database = new mysqli("localhost", "root", "", "playpal");                   //NOSONAR
-        $registrationStatement = $database->prepare(
-          "INSERT INTO user (Username, Email, Password, FirstName, LastName, Gender, Biography, PersonalWebsite, Phonenumbers, Name)
-          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $usrObj = jSONtoUser(file_get_contents("php://input"));
+    $report = performCredentialReport($usrObj);
+    if ($report->allTestPassed()) {
+      mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+      $database = new mysqli("localhost", "root", "", "playpal");                   //NOSONAR
+      $registrationStatement = $database->prepare(
+        "INSERT INTO user (Username, Email, Password, FirstName, LastName, Gender, Biography, PersonalWebsite, Pfp, Phonenumbers)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $username = $usrObj->getUsername();
+        $email = $usrObj->getEmail();
+        $password = $usrObj->getPassword();
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $firstname = $usrObj->getFirstName();
+        $lastname = $usrObj->getLastName();
+        $gender = $usrObj->getGender();
+        $biography = $usrObj->getBiography();
+        $personalWebsite = $usrObj->getPersonalWebsite();
+        $pfp = $usrObj->getPfp();
+        $phoneNumbers = $usrObj->getPhoneNumbers();
         $registrationStatement->bind_param("sssssissss",
-          $usrObj->getUsername(), $usrObj->getEmail(), $usrObj->getPassword(),
-          $usrObj->getFirstName(), $usrObj->getLastName(), $usrObj->getGender(),
-          $usrObj->getBiography(), $usrObj->getPersonalWebsite(),
-          $usrObj->getPhoneNumbers(), "noname");
-        $registrationStatement->execute();
-        // Here a query should be performed
+        $username, $email, $password, $firstname, $lastname,
+        $gender, $biography, $personalWebsite, $pfp, $phoneNumbers);
+
+        if (!$registrationStatement->execute()) {
+          throw new ApiError("Internal Server Error", 500,
+          "Error while contacting database", 500);
+        }
+        exit(generateJSONResponse(200, "Ok", generateToken(32), "authToken"));
       }
       exit(generateJSONResponse(401, "Invalid Information", $report));
-    } catch (Error $thrownError) {
-      throw new ApiError("Ok", 200, "Invalid JSON Format", 401);
-    }
   } catch (ApiError $thrownError) {
     header($_SERVER["SERVER_PROTOCOL"] . " " . $thrownError->getCode() . " " . $thrownError->getMessage());
     die(generateJSONResponse($thrownError->getApiErrorCode(), $thrownError->getApiMessage()));
   }
+
