b8f098e307fe4114776209d397fa3ca9605e46ea
Leonardo Marcaccio <Leonardo Marcaccio>
2024-02-01 15:21:19 +0100

    Added community functions

--- api/community/communitites.php
+++ api/community/communitites.php
@@ -44,6 +44,74 @@
     }
   }
 
+  function createCommunity($communityName, $image, $description, $founderUsername, mysqli $database) {
+    $query = $database->prepare("INSERT INTO community VALUES(?, ?, ?, ?)");
+    $query->bind_param("ssss", $communityName, $image, $description, $founderUsername);
+    if (!$query->execute()) {
+      throw new ApiError("Internal Server Error", 500,                
+        DB_CONNECTION_ERROR, 500);
+    }
+  }
+
+  function createPost($content, $title, $image, $communityName, $username, mysqli $database) {
+    $query = $database->prepare("INSERT INTO community VALUES(NOW(), ?, ?, ?, ?, ?)");
+    $query->bind_param("sssss", $content, $title, $image, $communityName, $username);
+    if (!$query->execute()) {
+      throw new ApiError("Internal Server Error", 500,                
+        DB_CONNECTION_ERROR, 500);
+    }
+  }
+
+  function createComment($postID, $content, $username, mysqli $database) {
+    $query = $database->prepare("INSERT INTO comment VALUES(NOW(), ?, ?)");
+    $query->bind_param("ss", $content, $username);
+    if (!$query->execute()) {
+      throw new ApiError("Internal Server Error", 500,                
+        DB_CONNECTION_ERROR, 500);
+    }
+
+    $query = $database->prepare("SELECT commentID FROM comment LIMIT 1 WHERE username=? ORDER BY date DESC");
+    $query->bind_param("s", $username);
+    if (!$query->execute()) {
+      throw new ApiError("Internal Server Error", 500,                
+        DB_CONNECTION_ERROR, 500);
+    }
+    
+    $commentID = $query->get_result();
+    
+    $query = $database->prepare("INSERT INTO answer VALUES(?, ?)");
+    $query->bind_param("ss", $commentID, $postID);
+    if (!$query->execute()) {
+      throw new ApiError("Internal Server Error", 500,                
+        DB_CONNECTION_ERROR, 500);
+    }
+  }
+
+  function createSubcomment($originID, $content, $username, mysqli $database) {
+    $query = $database->prepare("INSERT INTO community VALUES(NOW(), ?, ?)");
+    $query->bind_param("ss", $content, $username);
+    if (!$query->execute()) {
+      throw new ApiError("Internal Server Error", 500,                
+        DB_CONNECTION_ERROR, 500);
+    }
+
+    $query = $database->prepare("SELECT commentID FROM comment LIMIT 1 WHERE username=? ORDER BY date DESC");
+    $query->bind_param("s", $username);
+    if (!$query->execute()) {
+      throw new ApiError("Internal Server Error", 500,                
+        DB_CONNECTION_ERROR, 500);
+    }
+    
+    $commentID = $query->get_result();
+    
+    $query = $database->prepare("INSERT INTO subcomment VALUES(?, ?)");
+    $query->bind_param("ss", $commentID, $originID);
+    if (!$query->execute()) {
+      throw new ApiError("Internal Server Error", 500,                
+        DB_CONNECTION_ERROR, 500);
+    }
+  }
+
   function communityGetRequest() {
     if(!isset($_SERVER['type'])
       && !isset($_SERVER['target'])) {
@@ -67,4 +135,38 @@
         //createCommunityPage($targetCommunityName, $requestBody, $pages, $maxPerPage);
       break;
     }
+  }
+
+  function getCommunities($targetCommunityName, $requestBody, $pages, $maxPerPage, mysqli $database) {
+    $query = $database->prepare("SELECT * FROM community WHERE name=?");
+    $query->bind_param("s", $targetCommunityName);
+    if (!$query->execute()) {
+      throw new ApiError("Internal Server Error", 500,                
+        DB_CONNECTION_ERROR, 500);
+    }
+    return $query->get_result();
+  }
+
+  function getCommunityPost($targetCommunityName, $requestBody, $pages, $maxPerPage, mysqli $database) {
+    $n = $pages*$maxPerPage;
+    $query = $database->prepare("SELECT * FROM post LIMIT ? WHERE name=? ORDER BY date DESC");
+    $query->bind_param("is", $n, $targetCommunityName);
+    if (!$query->execute()) {
+      throw new ApiError("Internal Server Error", 500,                
+        DB_CONNECTION_ERROR, 500);
+    }
+
+    return $query->get_result();
+  }
+
+  function getPostComment($targetPostID, $requestBody, $pages, $maxPerPage, mysqli $database) {
+    $n = $pages*$maxPerPage;
+    $query = $database->prepare("SELECT * FROM answer LIMIT ? WHERE postID=? ORDER BY date DESC");
+    $query->bind_param("is", $n, $targetPostID);
+    if (!$query->execute()) {
+      throw new ApiError("Internal Server Error", 500,                
+        DB_CONNECTION_ERROR, 500);
+    }
+
+    return $query->get_result();
   }
