32b63888979c8a1aa1781db7ccf41dfec99a9a22
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-02-02 21:45:02 +0100

    Relocated registration module to a proper location

---
+++ components/registration/registration.js
@@ -0,0 +1,24 @@
+let optinalForm = document.getElementById("optionalForm");
+let registrationForm = document.getElementById("registrationForm")
+let addExtra = document.getElementById("optional");
+let back = document.getElementById("back");
+
+optinalForm.style.visibility = 'hidden';
+
+addExtra.addEventListener("click", () => {
+  registrationForm.style.visibility = 'hidden';
+  optinalForm.style.visibility = 'visible';
+  optinalForm.parentNode.insertBefore(optinalForm, optionalForm.parentNode.firstChild);
+});
+
+back.addEventListener("click", () => {
+  registrationForm.style.visibility = 'visible';
+  optinalForm.style.visibility = 'hidden';
+  registrationForm.parentNode.insertBefore(registrationForm, registrationForm.parentNode.firstChild);
+});
+
+addExtra.addEventListener("click", () => {
+  registrationForm.style.visibility = 'hidden';
+  optinalForm.style.visibility = 'visible';
+  optinalForm.parentNode.insertBefore(optinalForm, optionalForm.parentNode.firstChild);
+});
