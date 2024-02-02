1b67c7e565bce3112fc40aefb852c277b156d3ac
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-02-02 21:36:28 +0100

    Added missing id field in Post class

--- api/utility/classes/data/community/Post.php
+++ api/utility/classes/data/community/Post.php
@@ -6,14 +6,16 @@
     private $imageUrl;
     private $name;
     private $username;
+    private $id;
 
-    public function __construct($date, $content, $title, $name, $username, $imageUrl = null) {
+    public function __construct($date, $content, $title, $name, $username, $imageUrl = null, $id = null) {
       $this->date = $date;
       $this->content = $content;
       $this->title = $title;
       $this->imageUrl = $imageUrl;
       $this->name = $name;
       $this->username = $username;
+      $this->id = $id;
     }
 
     public function getDate() {
@@ -34,6 +36,9 @@
     public function getUsername() {
       return $this->username;
     }
+    public function getId() {
+      return $this->id;
+    }
 
     public function jsonSerialize() {
       return ['date' => $this->date,
@@ -41,6 +46,7 @@
         'title' => $this->title,
         'imageUrl' => $this->imageUrl,
         'name' => $this->name,
-        'username' => $this->username];
+        'username' => $this->username,
+        'id' => $this->id];
     }
   }
