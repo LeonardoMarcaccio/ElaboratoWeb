4a208d37a76a4adee9274efc7fd874d7e58bda2b
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2024-02-05 19:49:49 +0100

    Fixed chat and search UI

--- components/messages/messages.js
+++ components/messages/messages.js
@@ -1,20 +1,29 @@
+let form = document.createElement("form");
+let bar = document.createElement("textarea");
+let send = document.createElement("input");
+bar.placeholder = "Message";
+send.value = "Send";
+send.type = "button";
+form.appendChild(bar);
+form.appendChild(send);
+
 function loadChat() {
     let messageCount = 19;
     let chat = document.getElementById("page-dm-upper");
-    let user = document.createElement("textarea");
+    let user = document.createElement("p");
     user.textContent = chatCache.textContent;
-    user.disabled = true;
     user.className = "user-title";
     user.id = "user-dm";
-    mainGlobalVariables.page.mainContentPage.parentNode.insertBefore(user, mainGlobalVariables.page.mainContentPage);
+    mainGlobalVariables.page.mainContentHeading.appendChild(user);
     
     for (let i=0; i<messageCount; i++) {
-        let tmp = document.createElement("textarea");
+        let tmp = document.createElement("p");
         tmp.textContent = "Test-" + (i+1).toString();
-        tmp.disabled = true;
         tmp.className = i%2 == 0 ? "message-sent" : "message-received";
         chat.appendChild(tmp);
     }
+
+    mainGlobalVariables.page.mainContentFooting.appendChild(form);
 }
 
 chatLoader.updateLoadMethod(loadChat);
--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,8 +1,9 @@
 mainPageLoader = new PageLoader(mainGlobalVariables.page.mainContentPage, 0);
 headPageLoader = new PageLoader(mainGlobalVariables.page.mainContentHeading, 0);
+footPageLoader = new PageLoader(mainGlobalVariables.page.mainContentFooting, 0);
 generalButtonHandler = new ButtonHandler(null, ["feed", "search", "post", "chat", "profile"], "nav-", "mainContentPageChange", null);
 generalButtonHandler.activate();
-mainButtonHandler = new ButtonHandler(mainPageLoader, ["feed", "post"], "nav-", "mainContentPageChange", [headPageLoader]);
+mainButtonHandler = new ButtonHandler(mainPageLoader, ["feed", "post", "chat"], "nav-", "mainContentPageChange", [headPageLoader, footPageLoader]);
 mainButtonHandler.activate();
-headButtonHandler = new ButtonHandler(headPageLoader, ["search", "chat", "profile"], "nav-", "mainContentPageChange", [mainPageLoader]);
+headButtonHandler = new ButtonHandler(headPageLoader, ["search", "profile"], "nav-", "mainContentPageChange", [mainPageLoader, footPageLoader]);
 headButtonHandler.activate();
