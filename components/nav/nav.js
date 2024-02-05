a9344f1cae13991dfffe7bdfaa3febaf1e319cd6
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-02-05 21:36:56 +0100

    Fixed error in nav.js

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,8 +1,11 @@
-mainPageLoader = new PageLoader(mainGlobalVariables.page.mainContentPage, 0);
-headPageLoader = new PageLoader(mainGlobalVariables.page.mainContentHeading, 0);
-generalButtonHandler = new ButtonHandler(null, ["feed", "search", "post", "chat", "profile"], "nav-", "mainContentPageChange", null);
+let mainPageLoader = new PageLoader(mainGlobalVariables.page.mainContentPage, 0);
+let headPageLoader = new PageLoader(mainGlobalVariables.page.mainContentHeading, 0);
+let generalButtonHandler = new ButtonHandler(null,
+    ["feed", "search", "post", "chat", "profile"], "nav-", "mainContentPageChange", null);
 generalButtonHandler.activate();
-mainButtonHandler = new ButtonHandler(mainPageLoader, ["feed", "post"], "nav-", "mainContentPageChange", [headPageLoader]);
-mainButtonHandler.activate();
-headButtonHandler = new ButtonHandler(headPageLoader, ["search", "chat", "profile"], "nav-", "mainContentPageChange", [mainPageLoader]);
-headButtonHandler.activate();
+let mainButtonHandler = new ButtonHandler(mainPageLoader,
+    ["feed", "post"], "nav-", "mainContentPageChange", [headPageLoader]);
+    mainButtonHandler.activate();
+let headButtonHandler = new ButtonHandler(headPageLoader,
+    ["search", "chat", "profile"], "nav-", "mainContentPageChange", [mainPageLoader]);
+    headButtonHandler.activate();
