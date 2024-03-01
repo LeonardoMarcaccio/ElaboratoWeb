a835166c15abb353e00c99456ad5a410e9dc0758
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-03-01 17:16:47 +0100

    Performed fixes on logic

--- contenthandling.js
+++ contenthandling.js
@@ -1,3 +1,9 @@
+const contentHandlingConsts = {
+  htmlExtensions: ["html", "htm"],
+  cssExtensions: ["css"],
+  jsExtensions: ["js"]
+}
+
 /**
  * Class for an ElementHandler that helps to easily handle content inside
  * html elements of instances of the same class recursively.
@@ -10,7 +16,7 @@ class ElementHandler {
    */
   constructor(targetElement) {
     this.targetElement = targetElement;
-    this.recurse = targetElement instanceof HTMLElement;
+    this.recurse = !(targetElement instanceof HTMLElement);
   }
 
   /**
@@ -34,6 +40,15 @@ class ElementHandler {
           this.targetElement.appendChild(content);
         });
         await loadTask;
+      } else if (content instanceof String) {
+        let tmpElement = document.createElement("template");
+        tmpElement.innerHTML(content);
+        let tmpChilds = tmpElement.childNodes();
+        this.addContent(tmpChilds);
+      } else if (content instanceof Array) {
+        for (let singleElement in content) {
+          await this.addContent(singleElement);
+        }
       } else {
         throw new Error("Wrong type of content added!");
       }
@@ -57,7 +72,7 @@ class ElementHandler {
  * Class for an AssetLoader that can request content from an external server.
  */
 class AssetLoader {
-  constructor(sourceFolder) {
+  constructor(sourceFolder = "") {
     this.sourceFolder = sourceFolder;
   }
   /**
@@ -66,18 +81,36 @@ class AssetLoader {
    * @returns String containing the request's content
    * @throws Error if the content could not be retrieved
    */
-  async loadAsset(assets) {
+  async loadAsset(assets, configuration = {literalElement: true, loadHtml: false, loadCss: false, loadJs: false}) {
     let result;
-    if (Array.isArray(assets)) {
+    let targetAssets;
+    if (configuration.literalElement) {
+      targetAssets = assets;
+    } else {
+      if (assets instanceof Array) {
+
+      } else if (assets instanceof String) {
+        let tmpString
+      }
+    }
+    if (Array.isArray(targetAssets)) {
       result = Array();
-      for (let asset in assets) {
+      for (let asset in targetAssets) {
         result.push(await fetch(this.sourceFolder + asset));
       }
-    } else if (typeof assets === 'string') {
-      result = await fetch(this.sourceFolder + asset);
+    } else if (targetAssets instanceof String) {
+      result = await fetch(this.sourceFolder + targetAssets);
     } else {
       throw new Error("Invalid parameter passed");
     }
     return result;
+  }
+
+  /**
+   * 
+   * @param {String} asset 
+   */
+  extractAssetName(asset) {
+    return asset.slice(asset.lastIndexOf("/") + 1, asset.slice.lastIndexOf("."));
   }
 };
