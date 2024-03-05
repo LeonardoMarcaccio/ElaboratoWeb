a9d1d0821a177ccb7668a00b120249982b476145
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-03-05 12:37:51 +0100

    Bugfix

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -4,7 +4,6 @@ for (let btnElement of navButtons) {
   let eventName = btnElement.id.replace("nav-", "");
   btnElement.onclick = () => {
     let event = new CustomEvent(eventName, {detail: btnElement});
-    console.warn("generating new event and firing it");
     document.dispatchEvent(event);
   };
 };
