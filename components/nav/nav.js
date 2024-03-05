b08eb1ebf7c0f0a8d3ef8cc809b4cd50307f1a1a
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-03-05 09:52:34 +0100

    Fixed error in nav script and added script tag in nav

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,10 +1,10 @@
 let navButtons = document.getElementsByClassName("nav-button");
 
-navButtons.array.forEach(btnElement => {
+for (let btnElement of navButtons) {
   let eventName = btnElement.id.replace("nav-", "");
   btnElement.onclick = () => {
     let event = new CustomEvent(eventName, {detail: btnElement});
     console.warn("generating new event and firing it");
     document.dispatchEvent(event);
   };
-});
+};
