0b05c64f42c12e77d80b8e350ea51c7afd354156
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-04-03 17:29:22 +0200

    Not working push trying to fix various loading issues

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -4,8 +4,8 @@ let loader = new AssetLoader("/components/");
 for (let btnElement of navButtons) {
   let eventName = btnElement.id.replace("nav-", "");
   btnElement.onclick = async () => {
-    let page = await loader.loadAsset("/" + eventName + "/" + eventName, {literalElement: false, loadHtml: true, loadCss: false, loadJs: false});
-    mainHandler.contentHandling.setBodyContent(new ElementHandler(await page[0].text()).getContent());
+    //let page = await loader.loadAsset("/" + eventName + "/" + eventName, {literalElement: false, loadHtml: true, loadCss: false, loadJs: false});
+    //mainHandler.contentHandling.setBodyContent(new ElementHandler(await page[0].text()).getContent());
     let event = new CustomEvent(eventName, {detail: btnElement});
     document.dispatchEvent(event);
   };
